56b25638ee805dd2e5f21ccce37046a3657e23e3
axwii <axwii@users.noreply.github.com>
2024-11-28 10:30:24 +0100

    Update Footer and Header components for improved accessibility; add links to email, phone, and address, and adjust logo dimensions

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="bg-white p-4 shadow-md">
+    <header className="bg-white p-4">
       <div className="flex justify-center items-center">
         <Image
           src={"/static/images/KosanGascenterLogo.png"}
           alt="Kosan Gascenter Logo"
           priority
-          width={200}
-          height={200}
+          width={125}
+          height={38}
         />
       </div>
     </header>
