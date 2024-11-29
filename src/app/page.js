aa7310e5beca69f1b31b5a741d0b7e642a510993
axwii <axwii@users.noreply.github.com>
2024-11-29 01:56:57 +0100

    Add horizontal margins to header elements in Home component for improved layout

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <main role="main">
       <header className="text-center my-4">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-4xl font-bold mx-4">
           Kosan Gasbestilling
         </h1>
-        <p className="text-lg text-gray-700 mt-2">
+        <p className="text-lg text-gray-700 mt-2 mx-4">
           Velkommen til Kosan Gascenter København og Nordsjælland's gasbestilling
         </p>
       </header>
