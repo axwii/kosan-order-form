ab98dc409de955aef422c6fd612be9ba079e8cbd
axwii <axwii@users.noreply.github.com>
2024-11-29 01:53:28 +0100

    Remove commented-out grid layout code in CustomerForm for cleaner code

--- src/app/components/CustomerForm.js
+++ src/app/components/CustomerForm.js
@@ -31,7 +31,6 @@ const CustomerForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 max-w-5xl mx-auto">
         <ProductCard
           name="5 kg Kosan Gas Light"
