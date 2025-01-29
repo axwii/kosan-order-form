3dfb9dfd7fbe2457e5030431faa1083433cc93dd
axel <axwii@users.noreply.github.com>
2025-01-29 01:24:26 +0100

    Refactor header and add order deadline notice in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,19 @@ export default function Home() {
   return (
     <main role="main">
       <header className="text-center my-4">
-        <h1 className="text-4xl font-bold mx-4">
-          Kosan Gasbestilling
-        </h1>
+        <h1 className="text-4xl font-bold mx-4">Kosan Gasbestilling</h1>
         <p className="text-lg text-gray-700 mt-2 mx-4">
-          Velkommen til Kosan Gascenter København og Nordsjælland's gasbestilling
+          Velkommen til Kosan Gascenter København og Nordsjælland's
+          gasbestilling
         </p>
       </header>
       <section>
         <CustomerForm />
+      </section>
+      <section>
+        <p className="mt-4 text-gray-700 text-center">
+          Alle bestillinger skal afgives inden kl. 12:00 for levering næste dag.
+        </p>
       </section>
     </main>
   );
