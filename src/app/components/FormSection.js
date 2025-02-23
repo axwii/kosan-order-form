5aa77919672c1ca98ba162a90eb8e6c6ceaf29d1
axel <axwii@users.noreply.github.com>
2025-02-23 01:19:07 +0100

    Add id attribute to FormSection for improved accessibility

--- src/app/components/FormSection.js
+++ src/app/components/FormSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FormSection = ({ title, children }) => (
-  <div className="max-w-lg m-4 mt-20 sm:mx-auto p-4 bg-white border border-gray-300 rounded">
+  <div id="order-section" className="max-w-lg m-4 mt-20 sm:mx-auto p-4 bg-white border border-gray-300 rounded">
     <h2 className="text-2xl font-bold text-center">{title}</h2>
     {children}
   </div>
